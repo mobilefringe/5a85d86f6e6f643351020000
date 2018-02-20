15ce1d3cf0467ab5aaa3affe043e222770478e00
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2018-02-20 12:02:29 -0500

    Pine Centre 1.0  /services.vue

--- services.vue
+++ services.vue
@@ -10,10 +10,25 @@
 			</div>
 		</div>
 		<div class="site_container inside_page_content">
-            <div class="margin_side_20" >
-                <div class="page_body description_text text_left" v-if="locale=='en-ca'" v-html="currentPage.body"></div>
-                <div class="page_body description_text text_left" v-else v-html="currentPage.body_2"></div>
-            </div>
+            <!--<div class="margin_side_20" >-->
+            <!--    <div class="page_body description_text text_left" v-if="locale=='en-ca'" v-html="currentPage.body"></div>-->
+            <!--    <div class="page_body description_text text_left" v-else v-html="currentPage.body_2"></div>-->
+            <!--</div>-->
+            <accordion :one-at-atime="checked" type="info">
+                <panel is-open type="primary">
+                    <strong slot="header"><u>Panel #1</u></strong>
+                ...
+              </panel>
+              <panel header="Panel #2">
+                ...
+              </panel>
+              <panel header="Panel #3">
+                ...
+              </panel>
+              <panel header="Panel #4">
+                ...
+              </panel>
+            </accordion>
         </div>
         <div style="padding:20px 0;"></div>
     </div>
