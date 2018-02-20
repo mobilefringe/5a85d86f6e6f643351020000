598e97ff17ef4e514ffb8b7e84e4c26716b336a6
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2018-02-20 12:21:24 -0500

    Pine Centre 1.0  /services.vue

--- services.vue
+++ services.vue
@@ -14,50 +14,47 @@
                 <div class="page_body description_text text_left" v-if="locale=='en-ca'" v-html="currentPage.body"></div>
                 <div class="page_body description_text text_left" v-else v-html="currentPage.body_2"></div>
             </div>
-           <div role="tablist">
-    <b-card no-body class="mb-1">
-        <b-card-header header-tag="header" class="p-1" role="tab">
-            <b-btn block href="#" v-b-toggle.accordion1 variant="info">Accordion 1</b-btn>
-        </b-card-header>
-        <b-collapse id="accordion1" visible accordion="my-accordion" role="tabpanel">
-            <b-card-body>
-                <p class="card-text">
-                    I start opened because <code>visible</code> is <code>true</code>
-                </p>
-                <p class="card-text">
-                    {{ text }}
-                </p>
-            </b-card-body>
-        </b-collapse>
-    </b-card>
-    <b-card no-body class="mb-1">
-        <b-card-header header-tag="header" class="p-1" role="tab">
-            <b-btn block href="#" v-b-toggle.accordion2 variant="info">Accordion 2</b-btn>
-        </b-card-header>
-        <b-collapse id="accordion2" accordion="my-accordion" role="tabpanel">
-            <b-card-body>
-                <p class="card-text">
-                    {{ text }}
-                </p>
-            </b-card-body>
-        </b-collapse>
-    </b-card>
-    <b-card no-body class="mb-1">
-        <b-card-header header-tag="header" class="p-1" role="tab">
-            <b-btn block href="#" v-b-toggle.accordion3 variant="info">Accordion 3</b-btn>
-        </b-card-header>
-        <b-collapse id="accordion3" accordion="my-accordion" role="tabpanel">
-            <b-card-body>
-                <p class="card-text">
-                    {{ text }}
-                </p>
-            </b-card-body>
-        </b-collapse>
-    </b-card>
-</div>
-      </b-collapse>
-    </b-card>
-  </div>
+            <div role="tablist">
+                <b-card no-body class="mb-1">
+                    <b-card-header header-tag="header" class="p-1" role="tab">
+                        <b-btn block href="#" v-b-toggle.accordion1 variant="info">Accordion 1</b-btn>
+                    </b-card-header>
+                    <b-collapse id="accordion1" visible accordion="my-accordion" role="tabpanel">
+                        <b-card-body>
+                            <p class="card-text">
+                                I start opened because <code>visible</code> is <code>true</code>
+                            </p>
+                            <p class="card-text">
+                                {{ text }}
+                            </p>
+                        </b-card-body>
+                    </b-collapse>
+                </b-card>
+                <b-card no-body class="mb-1">
+                    <b-card-header header-tag="header" class="p-1" role="tab">
+                        <b-btn block href="#" v-b-toggle.accordion2 variant="info">Accordion 2</b-btn>
+                    </b-card-header>
+                    <b-collapse id="accordion2" accordion="my-accordion" role="tabpanel">
+                        <b-card-body>
+                            <p class="card-text">
+                                {{ text }}
+                            </p>
+                        </b-card-body>
+                    </b-collapse>
+                </b-card>
+                <b-card no-body class="mb-1">
+                    <b-card-header header-tag="header" class="p-1" role="tab">
+                        <b-btn block href="#" v-b-toggle.accordion3 variant="info">Accordion 3</b-btn>
+                    </b-card-header>
+                    <b-collapse id="accordion3" accordion="my-accordion" role="tabpanel">
+                        <b-card-body>
+                            <p class="card-text">
+                                {{ text }}
+                            </p>
+                        </b-card-body>
+                    </b-collapse>
+                </b-card>
+            </div>
         </div>
         <div style="padding:20px 0;"></div>
     </div>
