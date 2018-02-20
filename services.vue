0354ee5e7705a8426aa333c6ffca998f9dbbdc10
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2018-02-20 12:14:10 -0500

    Pine Centre 1.0  /services.vue

--- services.vue
+++ services.vue
@@ -48,6 +48,7 @@
 </style>
 <script>
     define(["Vue", "vuex", "moment", "moment-timezone", "vue-moment", "bootstrap-vue"], function(Vue, Vuex, moment, tz, VueMoment, BootstrapVue) {
+        console.log(BootstrapVue);
         return Vue.component("services-component", {
             template: template, // the variable template will be injected,
             data: function() {
