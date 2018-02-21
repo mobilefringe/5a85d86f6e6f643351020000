9e81c448aea10f41b2d24f6d40d6eb058eaf884b
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2018-02-21 14:31:33 -0500

    Pine Centre 1.0  /find_us.vue

--- find_us.vue
+++ find_us.vue
@@ -41,7 +41,7 @@
 <!--</style>-->
 
 <script>
-    define(["Vue", "vuex", "moment", "moment-timezone", "vue-moment", "vue-meta", 'vee-validate'], function(Vue, Vuex, moment, tz, VueMoment, Meta, VeeValidate) {
+    define(["Vue", "vuex", "vue-meta", 'vee-validate'], function(Vue, Vuex, Meta, VeeValidate) {
         Vue.use(Meta);
         Vue.use(VeeValidate);
         return Vue.component("find-us-component", {
