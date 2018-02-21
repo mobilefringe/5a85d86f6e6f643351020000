5fc13a3d965eeffebe1f15922dd8300c1a48e836
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2018-02-21 11:03:06 -0500

    Pine Centre 1.0  /activities.vue

--- activities.vue
+++ activities.vue
@@ -70,20 +70,45 @@ Monday to Saturday – South Entrance open at 7:00am; remaining doors at 8:30am.
                         },
                         {
                             title: "About Pine for Play - Toddlers Play Area",
-                            text: ` For your shopping convenience, coin operated lockers are now available and located down the hallway with the pay phones near Guest Services. They are available for a small fee of $2 per use.
-Please review the Locker Usage Rules & Regulations located below and on the inside door of each locker.
+                            text: `Pine for Play
 
-1. Lockers are available to our shoppers for their convenience while at Pine Centre.
-2. There is a $2.00 fee per use (noting once you open your locker, you will be required to insert an additional $2.00 to re-use).
-3. Lockers are available during Pine Centre shopping hours only and will be emptied daily at closing times.
-4. Please ensure that you collect all of your belongings at the end of your visit.
-5. Any items left at the end of the day will be considered ‘lost’ and recorded and handled as per our ‘Lost and Found Policy’.
-6. Lockers are meant to store jackets, shopping bags and other personal items that cannot be managed while shopping.
-7. Pine Centre Mall is NOT responsible for any lost or stolen items.
-8. Any misuse of lockers including vandalism, storage of illegal items, or storage of items that are ‘perishable’ in nature or otherwise unsuitable for storage, may result in legal action.
-9. It is important to note that this area is under video surveillance and activities may be recorded.
-10. Please see the Guest Services to claim any items left behind, report damaged lockers or request additional information about our Lost & Found Policy or Video Surveillance Policy or call 250 563 3681.
-11. There is a $10 replacement fee for lost keys.`,
+Take off your shoes and relax with your child at our popular soft play area, located near the North Entrance/Guest Services.
+
+The Toddler Play Area will be closed every day from 2:00pm – 2:30pm for scheduled cleaning. 
+
+Why must my child wear socks in the Play Area?
+Health regulations require that everyone in the Play Area must wear socks. Wearing socks is sanitary and prohibits the spread of foot diseases such as Athlete’s Foot and Plantar warts.  Socks also offer the safest method of play for the design and material of the play area.
+
+Be prepared to play! Download (view or print) the Parent Acknowledgement and Rules & Regulations for your next visit! A blank window will open, which is normal. Closing the window will cancel the download.
+
+The colourful Toddler Play Area has been themed to reflect our wildlife and recreational lifestyles and incorporates the communities of Prince George, Vanderhoof, Quesnel and Mackenzie (a map of this region has been included in the carpet layout). It complements our tribute to ‘Celebrate Northern Living’ and further enhances our strong ties to the families and residents of Prince George and the surrounding area.
+
+Children under six years of age (and or under 42” in height) who are accompanied by a parent or guardian are invited to enjoy the play area. Children MUST be supervised at ALL times – no exceptions!
+
+Caregiver groups (eg. child care providers, pre-school groups) please ask for ‘guidelines for caregiver groups’.
+
+(Pine Centre Mall Youth & Children’s Policies are in full effect noting children twelve and under are NOT permitted in the mall OR the play area without a parent or legal guardian within arm’s reach at ALL times.)   
+
+Pine Centre follows the age guidelines as set by the Canadian Child Welfare Research Group.
+
+The following Rules and Regulations apply:
+
+Parent or guardian MUST SIGN-IN at the Customer Service Centre prior to entering.
+Playtime periods may be limited to thirty minutes during peak times.
+Adults, guardians and children MUST remove footwear. NO bare feet or shoes allowed. Socks in two different sizes are available in a nearby dispenser for $1 per pair.
+NO food or drinks are allowed in the toddler play area – exceptions are made for breastfeeding and baby-held bottle feeding. Should any leaks or spills occur please notify Customer Service immediately. This rule does NOT allow for young children to walk around with a bottle or lidded cup in the play area.
+Play safely. NO running, jumping, hitting, pushing or ANY other unsafe play.
+Child(ren) MUST NOT stand on the outer edge seating at any time.
+NO yelling, screaming or swearing. Use of profanities WILL NOT be tolerated.    Children are asked to use their “inside voice” in consideration of the other parents and children and the retailers located close by.
+The number of visitors at any particular time may be limited to twenty– for safety and comfort reasons.
+Any child(ren) exhibiting cold or flu-like symptoms (or any other ‘contagious’ condition(s)) may be refused access to the play area.
+NO changing of diapers in the play area. Change tables are provided in the Family Room in the Food Court or in either of the Women’s Washrooms.
+Child(ren) MUST BE closely supervised by an adult at all times while in the Toddler Play Area. DO NOT leave child(ren) unattended at ANY time for ANY reason. Parents and or guardians are fully responsible for supervising the child(ren) they have signed in and acknowledge that they play at their own risk.
+While every consideration has been given for safety and convenience, Pine Centre is not responsible for injuries or lost articles.
+Pine Centre reserves the right to refuse access to the play area if these rules are not observed.
+For rule details or assistance please see the Customer Service Centre.
+
+Please follow the rules to ensure that everyone can have a great time!`,
                             is_visible : false,
                             id: "accordion_2"
                         }
