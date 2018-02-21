07bafeb24c9c776717466fbcbd5ad43aa1b0c0f4
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2018-02-21 11:03:59 -0500

    Pine Centre 1.0  /activities.vue

--- activities.vue
+++ activities.vue
@@ -50,7 +50,7 @@
 <script>
     define(["Vue", "vuex", "moment", "moment-timezone", "vue-moment", "bootstrap-vue"], function(Vue, Vuex, moment, tz, VueMoment, BootstrapVue) {
         Vue.use(BootstrapVue);
-        return Vue.component("services-component", {
+        return Vue.component("activities-component", {
             template: template, // the variable template will be injected,
             data: function() {
                 return {
