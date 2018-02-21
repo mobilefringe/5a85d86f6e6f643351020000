4243cb910c9609ff2a38ee7534e69503e78c10ab
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2018-02-21 14:31:23 -0500

    Pine Centre 1.0  /find_us.vue

--- find_us.vue
+++ find_us.vue
@@ -68,7 +68,6 @@
             computed: {
                 ...Vuex.mapGetters([
                     'property',
-                    'timezone',
                     'findRepoByName'
                 ]),
             },
