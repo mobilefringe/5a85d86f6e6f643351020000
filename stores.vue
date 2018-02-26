a6641969b8f3119f55258225bf46fdb521057bd2
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2018-02-26 16:13:25 -0500

    Pine Centre 1.0  /stores.vue

--- stores.vue
+++ stores.vue
@@ -45,7 +45,7 @@
 						<div class="store_logo_container" :id="store.initial">
 							<router-link :to="'/stores/'+ store.slug">
 								<div v-if="_.includes(store.store_front_url_abs, 'missing')" class="no_store_logo">
-                                    <h3 style="margin: 0;text-align: center;position: absolute;left: 50%;top: 50%;transform: translate(-50%, -50%);width: 100%;font-size: 28px;font-weight: bold;">{{store.name}}</h3>
+                                    <h3 class-"store_name">{{store.name}}</h3>
 								</div>
 								<img v-else class="store_img" :style="store.initial_img" :src="store.store_front_url_abs"/>
 							
