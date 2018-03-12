0f3b2794e0358b6e238b1c7166b091f5d9ca160b
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2018-03-12 11:40:57 -0400

    Pine Centre 1.0  /job_details.vue

--- job_details.vue
+++ job_details.vue
@@ -152,7 +152,7 @@
                 updateCurrentJob (id) {
                     this.currentJob = this.findJobBySlug(id);
                     if (this.currentJob === null || this.currentJob === undefined){
-                        this.$router.replace({ name: '/'});
+                        this.$router.replace('/');
                     }
                 },
                 loadData: async function() {
