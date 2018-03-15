957d1be48cf265d153a824d0d0687e23bb47bc4d
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2018-03-15 14:28:54 -0400

    Pine Centre 1.0  /home.vue

--- home.vue
+++ home.vue
@@ -167,6 +167,9 @@
                         if(key == 3) {
                             value.mobile_order = 2;
                         }
+                        if(key == 1) { 
+                            value.mobile_order = 3;
+                        }
                     });
                     features = _.sortBy(features, [function(o) { return o.mobile_order; }]);
                     console.log(features);
