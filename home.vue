391bc37a758a29e9edb27cfebfd35eba0001957a
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2018-03-15 14:34:02 -0400

    Pine Centre 1.0  /home.vue

--- home.vue
+++ home.vue
@@ -34,6 +34,25 @@
                 </div>
                 
             </div>
+            <div v-masonry transition-duration="0.3s" item-selector=".grid-item" class="hidden_phone">
+                <div v-masonry-tile class="item" >
+                    <div v-for="feature in feature_items" :class="'grid-item ' + feature.masonry_class ">
+                    	<div  :class="{ 'ih-item circle effect19' : feature.no_hover_class}"> 
+                    	<!--class="ih-item circle effect19"-->
+                    		<router-link :to="feature.url">
+                    			<img :src="feature.image_url" alt="name">
+                    			<div class="info">
+                    				<div class="content">
+                    					<h3 v-if="locale=='en-ca'"> {{feature.name}} </h3>
+                    					<h3 v-else> {{feature.name_2}} </h3>
+                    				</div>
+                    			</div>
+                    		</router-link>
+                    	</div>
+                    </div>
+                </div>
+                
+            </div>
             <!--<div v-masonry-tile class="item visible_phone" >-->
             <!--        <div v-for="feature in mobile_feature_items" :class="'grid-item ' + feature.masonry_class ">-->
             <!--        	<div  :class="{ 'ih-item circle effect19' : feature.no_hover_class}"> -->
