5b0c1455f6e41459cc1bfbc8c57c32ccf57cb715
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2018-03-16 15:46:40 -0400

    Pine Centre 1.0  /jobs.vue

--- jobs.vue
+++ jobs.vue
@@ -112,7 +112,7 @@
                         value.description_short_2 = _.truncate(value.description_2, {
                             'length': 150
                         });
-                        if (value.store != null && value.store != undefined && _.includes(value.store.image_url, 'missing')) {
+                        if (value.store != null && value.store != undefined && _.includes(value.store.store_front_url_abs, 'missing')) {
                             value.store.store_front_url_abs = vm.property.default_logo_url;
                         }
                         else if (value.store == null || value.store == undefined) {
