15ec5e3b00415d0fdfefe33e7ebaadbd41c9e8e6
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2018-03-16 16:00:27 -0400

    Pine Centre 1.0  /social_links.vue

---
+++ social_links.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <a href="https://www.twitter.com/pinecentre" target="_blank"><i class="fa fa-twitter" aria-hidden="true"></i></a>
+        <a href="https://www.facebook.com/pinecentre" target="_blank"><i class="fa fa-facebook-official" aria-hidden="true"></i></a>
+        <a href="https://www.instagram.com/pine_centre_mall" target="_blank"><i class="fa fa-instagram" aria-hidden="true"></i></a>
+         
+    </div>
+    
+</template>
+<script>
+    define(["Vue", "vuex", "lightbox"], function(Vue, Vuex, Lightbox) {
+        Vue.use(Lightbox);
+        return Vue.component("social-links", {
+            template: template, // the variable template will be injected,
+        });
+    });
+</script>
