56365e262350353ada18f2479c88ced58f6a3b6d
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2018-03-19 13:04:44 -0400

    Pine Centre 1.0  /store_details.vue

--- store_details.vue
+++ store_details.vue
@@ -49,7 +49,7 @@
 					<div class="store_promo_container" v-if="promotions.length > 0">
 						<div class="promo_container_title text-left caps"></div>
 						<h4 v-if="currentStore.rich_description" class="store_dets_title caps margin_30">{{$t("promos_page.promotions")}}</h4>
-						<div class="row store_promo_dets text-left" v-for="promo in promotions">
+						<div class="row store_promo_dets text-left" v-for="promo in promotions" :key="promo.id">
 							<div class="col-sm-6 no_padding" >
 								<div class="promo_div_image">
 									<img v-lazy="promo.image_url" class="image" alt=""/>
