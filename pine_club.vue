01540ea953a550f7b5e45f6da484e73516b0db3d
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2018-03-19 12:54:14 -0400

    Pine Centre 1.0  /pine_club.vue

--- pine_club.vue
+++ pine_club.vue
@@ -69,17 +69,6 @@
 						</div>
 					</form>
                     
-                    <div id="send_contact_success" class="alert alert-success text-left" role="alert" v-show="formSuccess">
-                        <span class="glyphicon glyphicon-ok" aria-hidden="true"></span>
-                        <span class="sr-only">Success</span>
-                        Thank you for subscribing to Kids PinE-club!
-                    </div>
-                    <div id="send_contact_error" class="alert alert-danger text-left" role="alert" v-show="formError">
-                        <span class="glyphicon glyphicon-exclamation-sign" aria-hidden="true"></span>
-                        <span class="sr-only">Error:</span>
-                        There was an error when trying to submit your request. Please try again later.
-                    </div>
-                    
                 </div>
             </div>
             <div class="padding_top_40"></div>    
