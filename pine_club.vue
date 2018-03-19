bdbc8e0e5340144e1e3c69d02f24a0336d161eb6
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2018-03-19 12:56:46 -0400

    Pine Centre 1.0  /pine_club.vue

--- pine_club.vue
+++ pine_club.vue
@@ -184,10 +184,7 @@
                     $("#newsletter_email").val(this.form_data.email);
                     console.log("this.$route.query", this.$route.query);
                     if(this.$route.query.success == 'success') {
-                        
                         this.formSuccess = true;
-                        console.log('this.$router' ,this.$router);
-                    this.$router.replace('/newsletter');
                     }
                 }
             },
