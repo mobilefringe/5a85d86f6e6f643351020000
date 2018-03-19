49f03993efb948390e53ae989a0418fa0332d250
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2018-03-19 12:40:18 -0400

    Pine Centre 1.0  /pine_club.vue

--- pine_club.vue
+++ pine_club.vue
@@ -32,7 +32,7 @@
 						<div class="form-group">
 							<div class="col-sm-6 col-xs-12" :class="{'has-error': errors.has('email')}">
 								<label class="label" for="email">Email <span class="req_star"> *</span></label>
-								<input v-model="form_data.email" v-validate="'required|email'" class="form-control js-cm-email-input" :class="{'input': true}" name="email" type="email" placeholder="Email" data-vv-delay="500" data-vv-as="email" id="newsletter_email">
+								<input v-model="form_data.email" v-validate="'required|email'" class="form-control js-cm-email-input" :class="{'input': true}" name="name="cm-zlyluu-zlyluu"" type="email" placeholder="Email" data-vv-delay="500" data-vv-as="email" id="newsletter_email">
 								<span v-show="errors.has('email')" class="form-control-feedback">{{ errors.first('email') }}</span>
 							</div>
 							<div class="col-sm-6 col-xs-12 " :class="{'has-error': errors.has('postal')}">
