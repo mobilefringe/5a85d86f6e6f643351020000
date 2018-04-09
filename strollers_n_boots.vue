21ac9163c0a71dbe1085f10fa8ca30da81f5773e
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2018-04-09 15:08:40 -0400

    Pine Centre 1.0  /strollers_n_boots.vue

--- strollers_n_boots.vue
+++ strollers_n_boots.vue
@@ -24,7 +24,7 @@
 								<span v-show="errors.has('first_name')" class="form-control-feedback">{{ errors.first('first_name') }}</span>
 							</div>
 							<div class="col-sm-6 col-xs-12" :class="{'has-error': errors.has('last_name')}">
-								<label class="label" for="last_name">Guardian's Last Name <span class="req_star"> *</span></label>
+								<label class="label" for="last_name">Last Name <span class="req_star"> *</span></label>
 								<input v-model="form_data.parent_last_name" v-validate="'required:true'" class="form-control" :class="{'input': true}" name="last_name" type="text" placeholder="Last Name" data-vv-delay="500" data-vv-as="guardian's last name">
 								<span v-show="errors.has('last_name')" class="form-control-feedback">{{ errors.first('last_name') }}</span>
 							</div>
