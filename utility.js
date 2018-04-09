d0802152864b7a8526e242be28703a8bdfd9c157
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2018-04-09 15:54:19 -0400

    Pine Centre 1.0  /utility.js

--- utility.js
+++ utility.js
@@ -1,4 +1,4 @@
-define([], function () {
+define([], function() {
     return {
         rannumber() {
             var randnumber = '';
@@ -18,10 +18,38 @@ define([], function () {
             });
             return newObj;
         },
-        campaignMonitorCall(){
-            
+        campaignMonitorCall() {
+            // Get e-mail value.
+            email = $('input[type=email]', form).val();
+
+            // Build request data for tokenRequest.
+            request_data = "email=" + encodeURIComponent(email) + "&data=" + form_id;
+
+            // Prepare tokenRequest.
+            tokenRequest = new XMLHttpRequest();
+            tokenRequest.open('POST', 'https://createsend.com//t/getsecuresubscribelink', true);
+            tokenRequest.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+            tokenRequest.send(request_data);
+
+            // Ready state.
+            tokenRequest.onreadystatechange = function() {
+                if (this.readyState === 4) {
+                    if (this.status === 200) {
+                        // Having token and new submit url we can create new request to subscribe a user.
+                        subscribeRequest = new XMLHttpRequest();
+                        subscribeRequest.open('POST', this.responseText, true);
+                        subscribeRequest.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+                        subscribeRequest.send(form.serialize());
+                        // On ready state call response function.
+                        subscribeRequest.onreadystatechange = function() {
+                            c.response(subscribeRequest);
+                        }
+                    } else {
+                        c.response(tokenRequest);
+                    }
+                }
+            }
+
         }
     }
 });
-
-
