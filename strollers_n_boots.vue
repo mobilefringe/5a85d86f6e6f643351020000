ebf7105c67190d0d5044b1529d7089944b843661
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2018-04-09 15:28:42 -0400

    Pine Centre 1.0  /strollers_n_boots.vue

--- strollers_n_boots.vue
+++ strollers_n_boots.vue
@@ -161,6 +161,8 @@
                             // }
                             //format contests data for MM
                             // this.form_data.notes = this.form_data.child_first_name + " " + this.form_data.child_last_name;
+                            send_data = {};
+                        send_data.form_data = JSON.stringify(Utility.serializeObject(this.form_data));
                             this.$store.dispatch("CONTACT_US", send_data).then(res => {
                                 this.formSuccess = true;
                             }).catch(error => {
