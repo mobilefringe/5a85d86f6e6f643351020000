7cd434b90eb51c22408c7a4cfda84c0bf97bd77a
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2018-04-09 15:02:33 -0400

    Pine Centre 1.0  /strollers_n_boots.vue

--- strollers_n_boots.vue
+++ strollers_n_boots.vue
@@ -11,7 +11,7 @@
         <div class="site_container">
             <div class="row text-center">
                 <div class="col-sm-12">
-                    <div v-if="currentContest" v-html="currentContest.rich_description"></div>
+                    <div v-if="currentContest" v-html="currentContest.rich_description"> {{body}}</div>
                 </div>
             </div> 
             <div class="row"> 
