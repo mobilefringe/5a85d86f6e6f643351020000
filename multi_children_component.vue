8e70d2d724edc8a33cd839367a7787fa1a84cb47
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2018-04-11 12:04:38 -0400

    Pine Centre 1.0  /multi_children_component.vue

--- multi_children_component.vue
+++ multi_children_component.vue
@@ -18,8 +18,8 @@
 			</div>
 			<div class="col-sm-6 col-xs-12" :class="{'has-error': errors.has('gender')}"  style="padding-top: 20px;">
 				<label class="label" for="gender">Gender <span class="req_star"> *</span></label>
-				<radio name="gender" value="male" v-model="child_form_data.gender" key="">Male</radio>
-                <radio name="gender" value="female" v-model="child_form_data.gender">Female</radio>
+				<radio name="gender" value="male" v-model="child_form_data.gender" :key="randKey">Male</radio>
+                <radio name="gender" value="female" v-model="child_form_data.gender" :key="randKey">Female</radio>
                 <span v-if="genderError" class="form-control-feedback">Please choosed a gender</span>
 			</div>
 		</div>
