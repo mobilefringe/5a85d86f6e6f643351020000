599e86cea521b727bb65772902e0551d343e47c5
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2018-04-13 12:26:09 -0400

    Pine Centre 1.0  /mall_walking_club.vue

--- mall_walking_club.vue
+++ mall_walking_club.vue
@@ -206,7 +206,7 @@
                             send_data.form_data = JSON.stringify(Utility.serializeObject(contact_form));
                             this.$store.dispatch("CONTACT_US", send_data).then(res => {
                                
-                                this.campaignMonitorCall($('#strollers_n_boots'), '92D4C54F0FEC16E5ADC2B1904DE9ED1A283BF1E3C2E61D583B605E13F21FF0FF32C7715BAF2BA07E11A65475E7F27713134C2662AA252AA55B4872C0D7BA96E2', $('#send_contact_success'), this.form_data);
+                                this.campaignMonitorCall($('#strollers_n_boots'), '92D4C54F0FEC16E5ADC2B1904DE9ED1A283BF1E3C2E61D583B605E13F21FF0FF32C7715BAF2BA07E11A65475E7F27713134C2662AA252AA55B4872C0D7BA96E2', $('#send_contact_success'));
                                 this.formSuccess = true;
                                 
                             }).catch(error => {
