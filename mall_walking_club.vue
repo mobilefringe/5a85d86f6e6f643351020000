d7a9593c6e63f6b28504884677c7ec3cb51366de
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2018-04-13 11:41:54 -0400

    Pine Centre 1.0  /mall_walking_club.vue

--- mall_walking_club.vue
+++ mall_walking_club.vue
@@ -75,6 +75,12 @@
                                 <radio name="doc_approval" value="female" v-model="form_data.doc_approval">No</radio>
                                 <span v-if="genderError" class="form-control-feedback">Please choosed a gender</span>
                 			</div>
+                			<div class="col-sm-6 col-xs-12 " :class="{'has-error': errors.has('postal')}"  style="padding-top: 20px;">
+								<label class="label" for="postal">Postal Code <span class="req_star"> *</span></label>
+								<input v-model="form_data.postal_code" v-validate="'required:true'" class="form-control" :class="{'input': true}" name="cm-f-atyjrk" type="text" placeholder="Postal Code" data-vv-delay="500" data-vv-as="postal code">
+								<span v-show="errors.has('postal')" class="form-control-feedback">{{ errors.first('postal') }}</span>
+							</div>
+                			How many times per week do you plan to walk at the centre?
 						</div>
 						<div class="form-group account-btn text-left m-t-10 agreement">
 						    
