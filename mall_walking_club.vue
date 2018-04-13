f975ac9e28aeb07d7a37a02120a5312cebddb390
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2018-04-13 12:35:11 -0400

    Pine Centre 1.0  /mall_walking_club.vue

--- mall_walking_club.vue
+++ mall_walking_club.vue
@@ -70,10 +70,10 @@
 						<div class="form-group">
 						    <div class="col-sm-6 col-xs-12" :class="{'has-error': errors.has('howd_you_hear')}"  style="padding-top: 20px;">
                 				<label class="label" for="howd_you_hear">How did you hear about the Walking Club? <span class="req_star"> *</span></label>
-                				<radio name="cm-f-attkkd" value="Advertising" v-model="form_data.howd_you_hear">Advertising</radio>
-                                <radio name="cm-f-attkkd" value="Brochure" v-model="form_data.howd_you_hear">Brochure</radio>
-                                <radio name="cm-f-attkkd" value="Friends" v-model="form_data.howd_you_hear">Friends</radio>
-                                <radio name="cm-f-attkkd" value="female" v-model="form_data.howd_you_hear">Other</radio>
+                				<radio name="cm-f-attkkd" value="advertising" v-model="form_data.howd_you_hear">Advertising</radio>
+                                <radio name="cm-f-attkkd" value="brochure" v-model="form_data.howd_you_hear">Brochure</radio>
+                                <radio name="cm-f-attkkd" value="friends" v-model="form_data.howd_you_hear">Friends</radio>
+                                <radio name="cm-f-attkkd" value="other" v-model="form_data.howd_you_hear">Other</radio>
                                 <span v-if="heardError" class="form-control-feedback">Please choose an option</span>
                 			</div>
 						    <div class="col-sm-6 col-xs-12" :class="{'has-error': errors.has('doc_approval')}"  style="padding-top: 20px;">
