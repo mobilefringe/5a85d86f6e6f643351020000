e2d26ad0e1aaba38e912af90fc11f0aae228c6a5
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2018-04-13 11:48:41 -0400

    Pine Centre 1.0  /mall_walking_club.vue

--- mall_walking_club.vue
+++ mall_walking_club.vue
@@ -73,7 +73,7 @@
                 				<label class="label" for="doc_approval">Have you checked with your physician prior to walking at the centre? <span class="req_star"> *</span></label>
                 				<radio name="doc_approval" value="male" v-model="form_data.doc_approval">Yes</radio>
                                 <radio name="doc_approval" value="female" v-model="form_data.doc_approval">No</radio>
-                                <span v-if="genderError" class="form-control-feedback">Please choosed a gender</span>
+                                <span v-if="genderError" class="form-control-feedback">Please choose an option</span>
                 			</div>
                 			<div class="col-sm-6 col-xs-12" :class="{'has-error': errors.has('howd_you_hear')}"  style="padding-top: 20px;">
                 				<label class="label" for="howd_you_hear">How did you hear about the Walking Club? <span class="req_star"> *</span></label>
