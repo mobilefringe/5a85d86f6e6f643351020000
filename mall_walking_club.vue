1a17bc64ce7bff4289e216da2dc24f0627d721f6
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2018-04-13 12:31:19 -0400

    Pine Centre 1.0  /mall_walking_club.vue

--- mall_walking_club.vue
+++ mall_walking_club.vue
@@ -92,7 +92,6 @@
 						</div>
 						<p>
 						    RISK OF INJURY:  Persons entering upon the premises of the Centre for the purpose of being a registered walker and member of the Pine Centre Walking Club, do so at their own risk.  Neither the Centre, nor any of its owners, their beneficiaries, merchants or agents shall have any liability for injury. We advise all registered walkers to check with their physician to see if walking at Pine Centre Mall is right for you.
-
 						</p>
 						<div class="form-group account-btn text-left m-t-10 agreement">
 						    <div class="col-xs-12">
