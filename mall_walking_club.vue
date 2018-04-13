0bcf0d532624a943be6bf1b5443eb7a439a4eb73
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2018-04-13 12:11:37 -0400

    Pine Centre 1.0  /mall_walking_club.vue

--- mall_walking_club.vue
+++ mall_walking_club.vue
@@ -60,16 +60,17 @@
 								<input v-model="form_data.age" v-validate="'required:true'" class="form-control" :class="{'input': true}" name="cm-f-atyjrk" type="text" placeholder="Age" data-vv-delay="500" data-vv-as="age">
 								<span v-show="errors.has('postal')" class="form-control-feedback">{{ errors.first('postal') }}</span>
 							</div>
-							<div class="col-sm-6 col-xs-12" :class="{'has-error': errors.has('gender')}"  style="padding-top: 20px;">
+							
+                			
+						</div>
+						<div class="form-group">
+						    <div class="col-sm-6 col-xs-12" :class="{'has-error': errors.has('gender')}"  style="padding-top: 20px;">
                 				<label class="label" for="gender">Gender <span class="req_star"> *</span></label>
                 				<radio name="gender_" value="male" v-model="form_data.gender">Male</radio>
                                 <radio name="gender" value="female" v-model="form_data.gender">Female</radio>
                                 <span v-if="genderError" class="form-control-feedback">Please choose a gender</span>
                 			</div>
-                			
-						</div>
-						<div class="form-group">
-						<div class="col-sm-6 col-xs-12 " :class="{'has-error': errors.has('num_walks')}"  style="padding-top: 20px;">
+						    <div class="col-sm-6 col-xs-12 " :class="{'has-error': errors.has('num_walks')}"  style="padding-top: 20px;">
 								<label class="label" for="num_walks">How many times per week do you plan to walk at the centre?<span class="req_star"> *</span></label>
 								<input v-model="form_data.num_walks" v-validate="'required:true'" class="form-control" :class="{'input': true}" name="cm-f-atyjrk" type="text" placeholder="Postal Code" data-vv-delay="500" data-vv-as="postal code">
 								<span v-show="errors.has('num_walks')" class="form-control-feedback">{{ errors.first('num_walks') }}</span>
