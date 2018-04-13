0b41ca5925a8b7d683cf37479152db9322bf45c3
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2018-04-13 11:53:45 -0400

    Pine Centre 1.0  /mall_walking_club.vue

--- mall_walking_club.vue
+++ mall_walking_club.vue
@@ -95,6 +95,13 @@
 
 						</p>
 						<div class="form-group account-btn text-left m-t-10 agreement">
+						<div class="col-xs-12">
+						        <label class="checkbox">
+                                    <input name="agree_terms" required type="checkbox" >
+                                    I understand by checking the box, I acknowledge that I have read, understood, and accept the <a href="/pages/pinecentre-pine-kid-s-club-disclaimer-and-the-code-of-conduct" target="_blank">Disclaimer and the Code of Conduct</a>
+                                </label>
+                                <label class=""> <a href="https://www.morguard.com/privacy/"> Read our Privacy Policy here</a></label> 
+						    </div>
 							<div class="col-xs-12" style="margin-top: 20px;">
 								<button class="contest_btn" type="submit" :disabled="formSuccess">Submit</button>
 							</div>
