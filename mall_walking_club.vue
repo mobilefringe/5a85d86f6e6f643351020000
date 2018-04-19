d01a05bd697f669044ea18d5352d22a22fc45c4f
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2018-04-19 16:34:24 -0400

    Pine Centre 1.0  /mall_walking_club.vue

--- mall_walking_club.vue
+++ mall_walking_club.vue
@@ -156,7 +156,7 @@
                     this.currentPage = response[0].data;
                     this.currentContest = this.findContestBySlug('pinecentre-walking-club');
                     // /pages/pinecentre-stroller-s-n-boots
-                    var temp_repo = this.findRepoByName('Programs Banner');
+                    var temp_repo = this.findRepoByName('Mall Walker Banner');
                     if(temp_repo) {
                         this.pageBanner = temp_repo.images[0];
                     }
