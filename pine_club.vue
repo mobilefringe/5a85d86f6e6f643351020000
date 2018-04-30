353878f92f934d5a6b781e81b638263375485177
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2018-04-30 14:56:44 -0400

    Pine Centre 1.0  /pine_club.vue

--- pine_club.vue
+++ pine_club.vue
@@ -222,7 +222,7 @@
                                 //         console.log("SUCCESS");
                                 //     }
                                 // });
-                                this.campaignMonitorCall($('#subForm'), '92D4C54F0FEC16E5ADC2B1904DE9ED1A283BF1E3C2E61D583B605E13F21FF0FF32C7715BAF2BA07E11A65475E7F27713134C2662AA252AA55B4872C0D7BA96E2');
+                                this.campaignMonitorCall($('#subForm'), '92D4C54F0FEC16E5ADC2B1904DE9ED1A2424A7FAF2895AE27BC1E6B945831C3A814668D6DF314C6CFD5AD436FBA4357E38F8203633B221924D53F47AEFF534B1');
                                 form.preventDefault();
                             }
                         }
