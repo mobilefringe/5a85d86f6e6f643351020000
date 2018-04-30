4d2b442fee3be2855371f86731d40cdec32057e8
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2018-04-30 15:05:15 -0400

    Pine Centre 1.0  /pine_club.vue

--- pine_club.vue
+++ pine_club.vue
@@ -271,7 +271,7 @@
                                 // On ready state call response function.
                                 subscribeRequest.onreadystatechange = function() {
                                     if (subscribeRequest.readyState === 4) {
-                                        console.log(subscribeRequest.response);
+                                        console.log("subscribeRequest.response", subscribeRequest.response)
                                         if (_.includes(subscribeRequest.response, 'Thank You')) {
                                             vm.formSuccess = true;
                                         } else {
