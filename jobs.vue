4a3dff618113f6c606ba910bcd362cecf75eb45c
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2018-05-08 13:25:47 -0400

    Pine Centre 1.1  /jobs.vue

--- jobs.vue
+++ jobs.vue
@@ -17,11 +17,11 @@
 							</router-link>
 						</div>
 						<div class="col-sm-6 col-md-8 event_dets_container">
-							<h4 class="event_name caps"  v-if="locale=='en-ca'">{{promo.name}}</h4>
-							<h4 class="event_name caps"  v-else>{{promo.name_2}}</h4>
+							<h2 class="event_name caps"  v-if="locale=='en-ca'">{{promo.name}}</h2>
+							<h2 class="event_name caps"  v-else>{{promo.name_2}}</h2>
 							<div v-if="promo.jobable_type == 'Store'">
-    						    <h4 class="event_store_name caps" v-if="locale=='en-ca'">{{promo.store.name}}</h4>
-    						    <h4 class="event_store_name caps" v-else>{{promo.store.name_2}}</h4>
+    						    <h3 class="event_store_name caps" v-if="locale=='en-ca'">{{promo.store.name}}</h3>
+    						    <h3 class="event_store_name caps" v-else>{{promo.store.name_2}}</h3>
     						</div>
 							<div class="event_thick_line"></div>
 							<p class="event_dates">{{promo.start_date | moment("MMM D", timezone)}} - {{promo.end_date | moment("MMM D", timezone)}}</p>
