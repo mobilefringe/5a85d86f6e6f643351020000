a925ecaab7d43b69c050170b49bdd0e436908277
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2018-05-08 13:08:55 -0400

    Pine Centre 1.1  /promotions.vue

--- promotions.vue
+++ promotions.vue
@@ -16,7 +16,7 @@
 							<img v-lazy="promo.store.store_front_url_abs"  class="event_image image" :alt="'Click here to view ' + promo.name"/>
 						</div>
 						<div class="col-sm-6 col-md-8 event_dets_container">
-							<h4 class="event_name caps" v-if="locale=='en-ca'">{{promo.name}}</h4>
+							<h2 class="event_name caps" v-if="locale=='en-ca'">{{promo.name}}</h2>
 							<h4 class="event_name caps" v-else>{{promo.name_2}}</h4>
 							<div v-if="promo.promotionable_type == 'Store'">
 							    <h4 class="event_store_name caps" v-if="locale=='en-ca'">{{promo.store.name}}</h4>
