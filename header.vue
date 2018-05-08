6c1cafb93df7436e86ca2d36125f4444c68ff6c2
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2018-05-08 11:05:36 -0400

    Pine Centre 1.1  /header.vue

--- header.vue
+++ header.vue
@@ -82,7 +82,7 @@
     						    <li v-for="item in menu_items" class="menu_item">
     						        <router-link v-if="item.sub_menu" to="#">{{$t(item.name)}}</router-link>
     						        <router-link v-else :to="item.href">{{$t(item.name)}}</router-link>
-    						        <ul v-if="item.sub_menu">
+    						        <ul v-if="item.sub_menu" class="dropdown">
     						            <li v-for="sub_menu in item.sub_menu" class="dropdown_item">
     						                <router-link :to="sub_menu.href">{{$t(sub_menu.name)}}</router-link>
     						            </li>
