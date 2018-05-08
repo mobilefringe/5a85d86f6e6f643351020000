a224ead4d1f1b50fb54f8953379f9d6d64620c80
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2018-05-08 15:24:22 -0400

    Pine Centre 1.1  /pine_club.vue

--- pine_club.vue
+++ pine_club.vue
@@ -41,7 +41,7 @@
 						</div>
 						<div class="form-group">
 							<div class="col-sm-6 col-xs-12" :class="{'has-error': errors.has('cm-zlyluu-zlyluu')}">
-								<label class="label" for="cm-zlyluu-zlyluu">Email <span class="req_star"> *</span></label>
+								<label class="label" for="newsletter_email">Email <span class="req_star"> *</span></label>
 								<input v-model="form_data.email" v-validate="'required|email'" class="form-control js-cm-email-input" :class="{'input': true}" name="cm-zlyluu-zlyluu" type="email" placeholder="Email" data-vv-delay="500" data-vv-as="email" id="newsletter_email">
 								<span v-show="errors.has('cm-zlyluu-zlyluu')" class="form-control-feedback">{{ errors.first('cm-zlyluu-zlyluu') }}</span>
 							</div>
