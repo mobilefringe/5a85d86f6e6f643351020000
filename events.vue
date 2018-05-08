5e28c0842166a444bc892be0c24ea7affe00206f
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2018-05-08 13:10:35 -0400

    Pine Centre 1.1  /events.vue

--- events.vue
+++ events.vue
@@ -19,8 +19,8 @@
 							</router-link>
 						</div>
 						<div class="col-sm-6 col-md-8 event_dets_container">
-							<h4 class="event_name caps" v-if="locale=='en-ca'">{{promo.name}}</h4>
-							<h4 class="event_name caps" v-else>{{promo.name_2}}</h4>
+							<h2 class="event_name caps" v-if="locale=='en-ca'">{{promo.name}}</h2>
+							<h2 class="event_name caps" v-else>{{promo.name_2}}</h2>
 							<div class="event_thick_line"></div>
 							<p class="event_dates">{{promo.start_date | moment("MMM D", timezone)}} - {{promo.end_date | moment("MMM D", timezone)}}</p>
 							<p class="event_desc" v-if="locale=='en-ca'">{{promo.description_short}}</p>
