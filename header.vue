f381e79e45a30bb432009548a9a111fdd375c6fb
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2018-05-08 11:44:28 -0400

    Pine Centre 1.1  /header.vue

--- header.vue
+++ header.vue
@@ -80,7 +80,7 @@
     					<nav id="primary_nav">
     						<ul>
     						    <li v-for="(item, index) in menu_items" class="menu_item" :class="{dropdown : item.sub_menu,open : item.open_dropdown}" @keyup.down="toggleDropdown(index)">
-    						        <router-link v-if="item.sub_menu" to="#" data-toggle="dropdown" class="dropdown-toggle" aria-haspopup="true" aria-expanded="false">{{$t(item.name)}}</router-link>
+    						        <router-link v-if="item.sub_menu" to="#" data-toggle="dropdown" class="dropdown-toggle" aria-haspopup="true" :aria-expanded="false">{{$t(item.name)}}</router-link>
     						        <router-link v-else :to="item.href">{{$t(item.name)}}</router-link>
     						        <ul v-if="item.sub_menu">
     						            <li v-for="sub_menu in item.sub_menu" class="dropdown_item">
@@ -140,7 +140,7 @@
             methods: {
                 toggleDropdown(index) {
                     console.log("menu is", this.menu_items[index]);
-                     this.menu_items[index].open_dropdown = truel
+                     this.menu_items[index].open_dropdown = true;
                 }
                 
             }
