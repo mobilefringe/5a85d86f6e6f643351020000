6f36152a3a094051038b953c830345ce2b44e013
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2018-05-08 10:29:27 -0400

    Pine Centre 1.1  /home.vue

--- home.vue
+++ home.vue
@@ -67,7 +67,8 @@
 		    </div>
 		    <div class="insta-feed-container">
                 <a v-for="(item, index) in instaFeed" :href="item.link" target="_blank" class="col-xs-6 col-sm-3 no_padding">
-                <div class="insta-feed-image "  v-lazy:background-image="item.images.standard_resolution.url"><p style="display:none;">{{item.caption.text}}</p></div>
+                <div class="insta-feed-image "  v-lazy:background-image="item.images.standard_resolution.url">
+                    <p style="display:none;">{{item.caption.text}}</p></div>
                 </a>
             </div>
             
