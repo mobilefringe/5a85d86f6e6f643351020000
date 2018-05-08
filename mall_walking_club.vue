2ec4474f4f8550702f14df7d9ecdf2b0e2e9dfe2
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2018-05-08 15:06:03 -0400

    Pine Centre 1.1  /mall_walking_club.vue

--- mall_walking_club.vue
+++ mall_walking_club.vue
@@ -64,8 +64,9 @@
 							<div class="col-sm-6 col-xs-12" :class="{'has-error': errors.has('gender')}"  style="padding-top: 20px;">
                 				<label class="label" for="gender">Gender <span class="req_star"> *</span></label>
                 				<input class="form-control" id="gender" type="hidden">
-                				<radio name="cm-f-attkki" value="male" v-model="form_data.gender">Male</radio>
+                				<fieldset><radio name="cm-f-attkki" value="male" v-model="form_data.gender">Male</radio>
                                 <radio name="cm-f-attkki" value="female" v-model="form_data.gender">Female</radio>
+                                </fieldset>
                                 <span v-if="genderError" class="form-control-feedback">Please choose a gender</span>
                 			</div>
 						</div>
