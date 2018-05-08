a7bf055d7a625abfdf73c06700b6303299e9d626
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2018-05-08 13:27:59 -0400

    Pine Centre 1.1  /events.vue

--- events.vue
+++ events.vue
@@ -14,9 +14,7 @@
 				<paginate name="events" v-if="events" :list="events" class="paginate-list margin-60" :per="4">
 					<div class="row event_container" v-for="(promo,index) in paginated('events')" :class="{ 'last': index === (paginated('events').length - 1) }" :key="promo.id">
 						<div class="col-sm-6 col-md-4 event_image_container">
-							<router-link :to="'/events/'+ promo.slug" class="event_learn_more">
-								<img v-lazy="promo.store.store_front_url_abs"  class="event_image image" :alt="'Click here to view ' + promo.name"/>
-							</router-link>
+							<img v-lazy="promo.store.store_front_url_abs"  class="event_image image" :alt="'Click here to view ' + promo.name"/>
 						</div>
 						<div class="col-sm-6 col-md-8 event_dets_container">
 							<h2 class="event_name caps" v-if="locale=='en-ca'">{{promo.name}}</h2>
