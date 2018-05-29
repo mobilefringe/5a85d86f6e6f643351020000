5df560cb329cf6953a27e6444d2e9f678c5dc2f8
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2018-05-29 17:56:27 -0400

    Pine Centre 1.0  /find_us.vue

--- find_us.vue
+++ find_us.vue
@@ -13,7 +13,7 @@
 			    <div class="col-md-12">
 			        <div class="padding_tb_30" v-if="currentPage" v-html="currentPage.body"></div>    
 			    </div>
-				<iframe title="Map" width="100%" height="450" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"  :src="'http://maps.google.nl/maps?q='+full_address  +'&amp;hl=en&amp;ie=UTF8&amp;t=v&amp;hnear='+full_address  +'&amp;z=17&amp;output=embed'"></iframe>
+				<iframe title="Map" width="100%" height="450" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"  :src="'https://maps.google.nl/maps?q='+full_address  +'&amp;hl=en&amp;ie=UTF8&amp;t=v&amp;hnear='+full_address  +'&amp;z=17&amp;output=embed'"></iframe>
 			</div>
 			<div class="padding_top_40"></div>
 		</div>
