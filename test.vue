9053f2295a866e1b5bde8d1d18ea31abd13a61b9
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2019-02-12 16:22:00 -0500

    Pine Centre 1.0  /test.vue

--- test.vue
+++ test.vue
@@ -3,3 +3,11 @@
         <iframe src='https://www.timetap.com/emb/214918' frameborder='0' width='840' height='500'></iframe>
     </div>
 </template>
+<script>
+    define(["Vue", "vuex", "lightbox"], function(Vue, Vuex, Lightbox) {
+        Vue.use(Lightbox);
+        return Vue.component("social-links", {
+            template: template, // the variable template will be injected,
+        });
+    });
+</script>
