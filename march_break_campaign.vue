2b8330f45a7d37969c408d7c0bcf2d72fd3187f3
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2019-02-27 16:48:04 -0500

    Pine Centre 1.0  /march_break_campaign.vue

--- march_break_campaign.vue
+++ march_break_campaign.vue
@@ -49,8 +49,8 @@
 						<multi-children-component v-show="showChild4" @childUpdated="updateChild" key="4" :id_num="4" :campaign="true"></multi-children-component>
 						<a @click="incrementChildren()" v-show="!noMoreChildren" style="margin: 10px auto 30px;display: inline-block;"> <i class="fa fa-plus"></i>Add Another Child</a>
 						<div class="form-group">
-    						<div class="col-sm-6 col-xs-12" :class="{'has-error': errors.has('timeslot1')}" >
-                				<label class="label" for="timeslot1">Gender <span class="req_star"> *</span></label>
+    						<div class="col-sm-6 col-xs-12" :class="{'has-error': errors.has('timeslot1')}">
+                				<label class="label" for="timeslot1">First Choice <span class="req_star"> *</span></label>
                 				<radio :name="'gender_'+randKey" value="male" v-model="child_form_data.gender" :key="randKey">Male</radio>
                                 <radio :name="'gender_'+ (randKey+1)" value="female" v-model="child_form_data.gender" :key="randKey+1">Female</radio>
                                 <span v-if="genderError" class="form-control-feedback">Please choosed a timeslot for your first choice</span>
